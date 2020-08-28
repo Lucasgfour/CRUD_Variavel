6927facf0761fb5ec06b7dc0021fb6f141d823ff
Lucasgfour <lucasmatheusg4@gmail.com>
2020-08-27 21:53:01 -0300

    Update para Banco de Dados
    Aula 27/08/2020

---
+++ src/org/produto/Conexao.java
@@ -0,0 +1,30 @@
+package org.produto;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+	private Connection conexao;
+	
+	public Conexao() {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver").newInstance();
+			String url = "jdbc:mariadb://localhost/libertas";
+			conexao = DriverManager.getConnection(url, "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void desconecta() {
+		try {
+			conexao.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConexao() {
+		return this.conexao;
+	}
+	
+}
--- src/org/produto/ProdutoDao.java
+++ src/org/produto/ProdutoDao.java
@@ -1,23 +1,58 @@
 package org.produto;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.LinkedList;
 
 public class ProdutoDao {
-	private static LinkedList<Produto> lista = new LinkedList<Produto>();
 	
-	public void inserir(Produto produto) {
-		lista.add(produto);
+	public void inserir(Produto p) {
+		Conexao con = new Conexao();
+		try {
+			String sql = "INSERT INTO produto(descricao, marca, preco_custo, preco_venda, saldo_estoque) VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement prep = con.getConexao().prepareStatement(sql);
+			prep.setString(1, p.getDescricao());
+			prep.setString(2, p.getMarca());
+			prep.setDouble(3, p.getPreco_custo());
+			prep.setDouble(4, p.getPreco_venda());
+			prep.setInt(5, p.getSaldo_estoque());
+			prep.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		con.desconecta();
 	}
 	
 	public String toTable() {
 		String saida = "";
-		for(Produto prod : lista) {
+		for(Produto prod : listar()) {
 			saida = saida + prod.toTable();
 		}
 		return saida;
 	}
 	
 	public LinkedList<Produto> listar() {
+		Conexao con = new Conexao();
+		LinkedList<Produto> lista = new LinkedList<Produto>();
+		try {
+			String sql = "SELECT descricao, marca, preco_custo, preco_venda, saldo_estoque FROM produto";
+			Statement instrucao = con.getConexao().createStatement();
+			ResultSet res = instrucao.executeQuery(sql);
+			while(res.next()) {
+				Produto p = new Produto();
+				p.setDescricao(res.getString("descricao"));
+				p.setMarca(res.getString("marca"));
+				p.setPreco_custo(res.getDouble("preco_custo"));
+				p.setPreco_venda(res.getDouble("preco_venda"));
+				p.setSaldo_estoque(res.getInt("saldo_estoque"));
+				lista.add(p);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		con.desconecta();
 		return lista;
 	}
 }
